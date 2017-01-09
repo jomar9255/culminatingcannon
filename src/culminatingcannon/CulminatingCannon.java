c67972963739e19961ce864c03fa3c52dc532e50
Will Pringle <vargsenpai@gmail.com>
2017-01-08 20:58:32 -0500

    Update CulminatingCannon.java

--- src/culminatingcannon/CulminatingCannon.java
+++ src/culminatingcannon/CulminatingCannon.java
@@ -14,11 +14,48 @@ public class CulminatingCannon {
         double v1=10, t=6,a=-9.8;
         
         
-        System.out.println("dislacement is: "+Δdv1taFORΔdy(v1,t,a));
-        System.out.println("initial vel is: "+Δdv1taFORv1(Δdv1taFORΔdy(v1,t,a),t,a));
+        System.out.println("dislacement is: "+Δdv1taOFΔdy(v1,t,a));
+        System.out.println("initial vel is: "+Δdv1taOFv1(Δdv1taOFΔdy(v1,t,a),t,a));
     }
     
-    public static double Δdv1taFORΔdy(double v1,double t,double a){
+    public static double ΔdΔtOFv(double Δd, double Δt){
+        // DECLARE A CONSTANT SPEED VARIABLE
+        double v;
+        
+        // CALCULATE FOR VELOCITY
+        v = Δd/Δt;
+        
+        // RETURN
+        return v;
+    }
+    
+    public static double ΔdvOFΔt(double Δd, double v){
+        // DECLARE A DELTA TIME VARIABLE
+        double Δt;
+        
+        // CALCULATE FOR DELTA TIME
+        Δt = Δd/v;
+        
+        // RETURN
+        return v;
+    }
+    
+    public static double ΔtvOFΔd(double Δt, double v){
+        // DECLARE A DELTA DISPLACEMENT
+        double Δd;
+        
+        // CALCULATE FOR DELTA DISPLACEMENT
+        Δd = Δt*v;
+        
+        // RETURN
+        return Δd;
+    }
+    
+    
+    // THIS IS SECTION 1 FOR PROJECTILE MOTION
+    // IT USES THE FORMULA d = v1*t + 1/2 * a * t *t
+    
+    public static double Δdv1taOFΔdy(double v1,double t,double a){
         // DECLARE Δ DISPLACEMENT VARIABLE
         double Δdy;
 
@@ -29,7 +66,7 @@ public class CulminatingCannon {
         return Δdy;
     }
     
-    public static double Δdv1taFORv1(double Δdy,double t,double a){
+    public static double Δdv1taOFv1(double Δdy,double t,double a){
         // DECLARE initial speed
         double v1;
 
@@ -40,4 +77,36 @@ public class CulminatingCannon {
         return v1;
     }
     
+    public static double Δdv1tv1OFa(double Δdy,double t,double v1){
+        // DECLARE ACCELERATION VARIABLE
+        double a;
+
+        // CALCULATE FOR initial speed
+        a=(2*(Δdy-(v1*t)))/Math.pow(t, 2);
+        
+        // RETURN
+        return a;
+    }
+    
+    
+    /* LOOK INTO THIS ASK ETHAN */
+    public static double Δdv1av1OFt(double Δdy,double a,double v1){
+        // DECLARE ACCELERATION VARIABLE
+        double t1, t2;
+        double trueTime = 999999; // set as wild number to verify for testing
+        
+        // CALCULATE FOR initial speed
+        t1=(-2*v1+Math.sqrt(Math.pow(2*v1, 2)-4*(a*(-2*Δdy))))/2*a;
+        t2=(-2*v1-Math.sqrt(Math.pow(2*v1, 2)-4*(a*(-2*Δdy))))/2*a;
+        
+        // CHECK WHICH ONE OF THE TIMES IS VALID AND SET THE TRUE TIME TO IT
+        if(t1<0){
+           trueTime=t1; 
+        }
+        else if(t2<0){
+            trueTime=t2;
+        }
+        
+        return trueTime;
+    }
 }
